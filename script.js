fbfaf3e03c2dd74cbbe027068f10c3d175e80f2e
Kate Kozelkova <kate.kozelkova@gmail.com>
2016-02-22 00:21:53 +0500

    babe

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 /**
- * Created by Кэтрин on 08.02.2016.
+ * Created by пїЅпїЅпїЅпїЅпїЅпїЅ on 08.02.2016.
  */
+console.log("qweqwe");
+VK.init(function() {
+    // API initialization succeeded
+    // Your code here
+    VK.api('users.get',{},function(data) {
+        if (data.response) {
+            // data.response is object
+            console.log(data.response)
+        }
+    });
+});
